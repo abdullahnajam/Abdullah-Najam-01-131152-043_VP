8d391b617a69945c95ed6807ac62dffd0fba1cf5
abdullahnajam <anajam55@gmail.com>
2018-05-18 00:15:36 +0500

    face detection

--- Attendence System/Attendence System/Attendance.Designer.cs
+++ Attendence System/Attendence System/Attendance.Designer.cs
@@ -34,21 +34,25 @@
             this.cameraCapture = new Emgu.CV.UI.ImageBox();
             this.viewButton = new System.Windows.Forms.Button();
             this.back = new System.Windows.Forms.Button();
+            this.getfaceImage = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.cameraCapture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getfaceImage)).BeginInit();
             this.SuspendLayout();
             // 
             // markAttendance
             // 
-            this.markAttendance.Location = new System.Drawing.Point(171, 298);
+            this.markAttendance.Location = new System.Drawing.Point(50, 379);
             this.markAttendance.Name = "markAttendance";
             this.markAttendance.Size = new System.Drawing.Size(120, 37);
             this.markAttendance.TabIndex = 1;
             this.markAttendance.Text = "Mark Attendance";
             this.markAttendance.UseVisualStyleBackColor = true;
+            this.markAttendance.Click += new System.EventHandler(this.markAttendance_Click);
             // 
             // registerFace
             // 
-            this.registerFace.Location = new System.Drawing.Point(171, 341);
+            this.registerFace.Location = new System.Drawing.Point(50, 422);
             this.registerFace.Name = "registerFace";
             this.registerFace.Size = new System.Drawing.Size(120, 32);
             this.registerFace.TabIndex = 2;
@@ -58,15 +62,15 @@
             // cameraCapture
             // 
             this.cameraCapture.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.cameraCapture.Location = new System.Drawing.Point(60, 12);
+            this.cameraCapture.Location = new System.Drawing.Point(12, 12);
             this.cameraCapture.Name = "cameraCapture";
-            this.cameraCapture.Size = new System.Drawing.Size(397, 266);
+            this.cameraCapture.Size = new System.Drawing.Size(581, 321);
             this.cameraCapture.TabIndex = 2;
             this.cameraCapture.TabStop = false;
             // 
             // viewButton
             // 
-            this.viewButton.Location = new System.Drawing.Point(171, 379);
+            this.viewButton.Location = new System.Drawing.Point(50, 460);
             this.viewButton.Name = "viewButton";
             this.viewButton.Size = new System.Drawing.Size(120, 37);
             this.viewButton.TabIndex = 3;
@@ -76,7 +80,7 @@
             // 
             // back
             // 
-            this.back.Location = new System.Drawing.Point(373, 412);
+            this.back.Location = new System.Drawing.Point(522, 540);
             this.back.Name = "back";
             this.back.Size = new System.Drawing.Size(75, 23);
             this.back.TabIndex = 4;
@@ -84,11 +88,32 @@
             this.back.UseVisualStyleBackColor = true;
             this.back.Click += new System.EventHandler(this.back_Click);
             // 
+            // getfaceImage
+            // 
+            this.getfaceImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.getfaceImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.getfaceImage.Location = new System.Drawing.Point(278, 339);
+            this.getfaceImage.Name = "getfaceImage";
+            this.getfaceImage.Size = new System.Drawing.Size(205, 198);
+            this.getfaceImage.TabIndex = 5;
+            this.getfaceImage.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(360, 550);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
             // Attendance
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(529, 451);
+            this.ClientSize = new System.Drawing.Size(609, 575);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.getfaceImage);
             this.Controls.Add(this.back);
             this.Controls.Add(this.viewButton);
             this.Controls.Add(this.cameraCapture);
@@ -98,7 +123,9 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Attendance_Load);
             ((System.ComponentModel.ISupportInitialize)(this.cameraCapture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getfaceImage)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -108,6 +135,8 @@
         private Emgu.CV.UI.ImageBox cameraCapture;
         private System.Windows.Forms.Button viewButton;
         private System.Windows.Forms.Button back;
+        private System.Windows.Forms.PictureBox getfaceImage;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Attendence System/Attendence System/Attendance.cs
+++ Attendence System/Attendence System/Attendance.cs
@@ -11,6 +11,7 @@ using Emgu.CV;
 using Emgu.Util;
 using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
+using System.Reflection;
 
 namespace Attendence_System
 {
@@ -71,25 +72,37 @@ namespace Attendence_System
         //bool captureInProgress;
         //Capture capture;
         HaarCascade haars;
+        Graphics facedraw;
+        Bitmap saveIamge;
         private void cameraOn(object sender, EventArgs e)
         {
 
-
+            //C:\\Users\\Ultron\\Documents\\Visual Studio 2015\\Projects\\Abdullah-Najam-01-131152-043_VP\\Attendence System\\Attendence System
             Image<Bgr, Byte> ImageFrame = cap.QueryFrame();
             cameraCapture.Image = ImageFrame;
-            haars = new HaarCascade("C:\\Users\\Ultron\\Documents\\Visual Studio 2015\\Projects\\Abdullah-Najam-01-131152-043_VP\\Attendence System\\Attendence System\\haarcascade_frontalface_alt_tree.xml");
+            string appStartPath= System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            haars = new HaarCascade(appStartPath+"\\haarcascade_frontalface_alt_tree.xml");
             Image<Gray, byte> grayframe = ImageFrame.Convert<Gray, byte>();
             var faces = grayframe.DetectHaarCascade(haars, 1.2, 3, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(25, 25))[0];
-            int TotalFaces = faces.Length;
+            Bitmap InputImage = grayframe.ToBitmap();
             
             foreach (var face in faces)
             {
                 ImageFrame.Draw(face.rect, new Bgr(Color.Red), 3);
-            }
+                saveIamge = new Bitmap(face.rect.Height,face.rect.Width);
+                facedraw = Graphics.FromImage(saveIamge);
 
+                facedraw.DrawImage(InputImage, 0, 0, face.rect, GraphicsUnit.Pixel);
+                
+            }
+            
             cameraCapture.Image = ImageFrame;
         }
-       
+
+        private void markAttendance_Click(object sender, EventArgs e)
+        {
+            getfaceImage.Image = saveIamge;
+        }
     }
 }
  
